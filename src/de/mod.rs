426459e9ae5f3edee5ad069f9a43f11a105beceb
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2025-10-29 17:26:27 -0700

    Dedupe the `impl_borrow_decode*` macros implementations
    `impl_borrow_decode` can be implemented by wrapping
    `impl_borrow_decode_with_context` and passing a `__Context` value.

--- src/de/mod.rs
+++ src/de/mod.rs
@@ -121,13 +121,7 @@ pub trait BorrowDecode<'de, Context>: Sized {
 #[macro_export]
 macro_rules! impl_borrow_decode {
     ($ty:ty $(, $($params:tt)*)?) => {
-        impl<'de, __Context $(, $($params)*)?> $crate::BorrowDecode<'de, __Context> for $ty {
-            fn borrow_decode<D: $crate::de::BorrowDecoder<'de, Context = __Context>>(
-                decoder: &mut D,
-            ) -> core::result::Result<Self, $crate::error::DecodeError> {
-                $crate::Decode::decode(decoder)
-            }
-        }
+        $crate::impl_borrow_decode_with_context!($ty, __Context, __Context $(, $($params)*)?);
     };
 }
 
